6915ca9667127f487be3954d10025a86d7d1daec
Shawn Garbett <shawn.garbett@vanderbilt.edu>
2019-01-29 10:41:06 -0600

    More code pruning related to cost

--- costs_ICER.R
+++ costs_ICER.R
@@ -3,23 +3,20 @@
 source('inputs_IGNITE.R')
 source("./clopidogrel/counters.R")
 source("./main/counters.R")
-counters <- c(counters.gen, counters.dapt, counters.warfarin)
-inputs$vN <- 200000
+counters  <- c(counters.gen, counters.dapt)
+#inputs$vN <- 200000
 
 ###########
 options("scipen"=100, "digits"=6)
 annual_discount_rate <- 0.03
 
-cont_discount_rate   <- -log(1-annual_discount_rate) # Yearly Time Scale
+cont_discount_rate <- -log(1-annual_discount_rate) # Yearly Time Scale
 discounted_cost <- function(start_day, end_day, base_yearly_cost, rate = cont_discount_rate)
 {
-  #base_yearly_cost*(exp(-rate*start_day/365) - exp(-rate*end_day/365))/rate 
-  
   base_yearly_cost / (exp(-rate*((end_day-start_day)/365)))
-  
 }
 
-discount_value = function(value,ar=annual_discount_rate,A,B)
+discount_value <- function(value,ar=annual_discount_rate,A,B)
 {
   r <- (1 + ar)^(1/365)-1
   (value/r)*(exp(-r*A)-exp(-r*B))
@@ -110,75 +107,4 @@ cost.qaly <- function(raw,inputs)
   c4 <- COST %>% group_by(name) %>% filter(cat==3) %>% dplyr::summarize(event=sum(dCOST))
   out <- QALY %>% left_join(c1,by="name") %>% left_join(c2,by="name") %>% left_join(c3,by="name") %>% left_join(c4,by="name")
   return(out)
-  #avgsum <- data.frame(    dQALY = sum(QALY$dQALY)/inputs$vN,
-   #                        dCOST = sum(COST$dCOST)/inputs$vN,
-    #                       dCOST.test = sum(COST$dCOST[COST$cat==1])/inputs$vN,
-     #                      dCOST.drug = sum(COST$dCOST[COST$cat==2])/inputs$vN,
-      #                     dCOST.event = sum(COST$dCOST[COST$cat==3])/inputs$vN) %>%
-    #mutate(NMB10k=dQALY*10000-dCOST,NMB50k=dQALY*50000-dCOST,NMB100k=dQALY*100000-dCOST) #add NMB calcuation
-  #return(avgsum)
 }
-
-
-
-#######ICER & C/E Plane
-#library(ggplot2)
-#library(ggrepel)
-#library(dplyr)
-#library(tidyr)
-
-icer <- function(results) 
-{
-  x <- results %>% arrange(dCOST) %>% mutate(ICER = (lag(dCOST)-dCOST)/(lag(dQALY)-dQALY)) 
-
-  #strong dominance (cross out strategies with a negative ICER)
-  str.dom <- NULL
-  if(any(x$ICER[-1]<0)==FALSE) {
-    x$dominated[2:(nrow(x))] = 0
-  } 
-  while(any(x$ICER[-1]<0))
-    {
-      y <- x %>% filter(ICER<0)
-      x <- x %>% filter(ICER>0 | is.na(ICER)) %>% arrange(dCOST) %>% mutate(ICER = (dCOST-lag(dCOST))/(dQALY-lag(dQALY)))
-      x$dominated[2:(nrow(x))] = 0
-      str.dom <- rbind.fill(str.dom, y)
-    }
-  if(is.null(str.dom)==FALSE) {str.dom <- str.dom %>% mutate(ICER=NA, dominated=1)}
-  
-  #extended dominance (cross out weakly dominated strategies until ICERs always increase with costs)
-  ext.dom <- NULL
-  while(any(order(x$ICER[-1])!=1:(nrow(x)-1))) 
-  {
-    r <- nrow(x)
-    x$ext.dominated <- NA
-    for (i in 2:(r-1)) {
-      x$ext.dominated[i] = as.integer(x$ICER[i] > x$ICER[i+1])   
-    }
-    y <- x %>% filter(ext.dominated==1)
-    x <- x %>% filter(ext.dominated==0 | is.na(ext.dominated)) %>% arrange(dCOST) %>% mutate(ICER = (dCOST-lag(dCOST))/(dQALY-lag(dQALY)))
-    ext.dom <- rbind.fill(ext.dom, y)
-  }
-  if(is.null(ext.dom)==FALSE) {ext.dom <- ext.dom %>% mutate(ICER=NA, ext.dominated=1) }
-
-  out = plyr::rbind.fill(x, str.dom, ext.dom) %>% arrange(dCOST)
-  out
-}
-
-### need to adjust labels for strategies
-ce_plane <- function(sum, title) {
-  dt <- sum
-  dt$line <- 1
-  if (any(names(dt) %in% "dominated")==TRUE)
-  {dt$line[dt$dominated==1] <- 0 }
-  if (any(names(dt) %in% "ext.dominated")==TRUE)
-  {dt$line[dt$ext.dominated==1] <- 0}
-  if (any(names(dt) %in% "Y")==TRUE) 
-  {dt <- dt %>% mutate(label=paste0(preemptive,"-",reactive,"-",X,"-",Y))} # With sensitivity analyses
-  if (any(names(dt) %in% "Y")==FALSE) 
-  {dt <- dt %>% mutate(label=paste0(preemptive,"-",reactive))}
-  ggplot(dt, aes(x=dQALY,y=dCOST)) + geom_point() + 
-    geom_line(data=dt[dt$line==1,]) + 
-    geom_label_repel(aes(label=label)) +
-    ggtitle(title)
-}
-
