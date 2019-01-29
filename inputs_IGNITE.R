c90ef0405a948bfbe13c105fd3e9b85eac68eeb9
Shawn Garbett <shawn@garbett.org>
2019-01-29 07:59:49 -0600

    More stripping out of simvastatin and warfarin

--- inputs_IGNITE.R
+++ inputs_IGNITE.R
@@ -19,8 +19,6 @@ realdol = function(nominal,year,base=2017)
 
 epsilon <- 0.000000000001
 
-
-
 clopidogrel = list(
   vPREDICTsens = 0.23,
   vPREDICTspec = 0.93,
@@ -187,144 +185,6 @@ clopidogrel = list(
 )
 
 
-simvastatin <- list(
-    vPREDICTsens = 0.23,
-    vPREDICTspec = 0.93,
-    vProbabilityRead = 1.00, # probability of physician using test results
-    vProbabilityReactive = 1.00, # Under reactive, probability of ordering test
-    
-    # Weibull for statin prescription
-    vScale = 13989.02,
-    vShape = 0.54,
-    
-    #
-    vMedMetabolizer  = 0.249,   # Prevalence of medium metabolizers
-    vPoorMetabolizer = 0.021,   # Prevalence of poor metabolizers
-  
-    vProbSimvastatinAlt = 1.00,  # Prob. of Alt | Variant
-    vProbSimStopMild = 0.23,  # Prob. of Stop | Mild Myo
-    vProbSimStopMod  = 0.23,  # Prob. of Stop | Mod Myo
-    vProbSimStopSev  = 1.00,  # Prob. of Stop | Sev Myo
- 
-    # 5-year Mild Myopathy Risks
-    vMildMyoBaseNoVar=1e-7, # No Drug Risk of mild myopathy
-    vMildMyoSimNoVar=0.05,  # Simvastatin Mild Myopathy Baseline Risk
-    vMildMyoSimMedVar=2.55,    # Rel Risk|Medium metabolizer
-    vMildMyoSimPoorVar=9.46,   # Rel Risk|Poor metabolizer
-    vMildMyoAltNoVar=0.05,  # Alternate Drug Mild Myopathy Baseline Risk
-    vMildMyoAltMedVar=1,    # Rel Risk|Medium metabolizer
-    vMildMyoAltPoorVar=1,    # Rel Risk|Poor metabolizer
-
-    # 5-year Moderate Myopathy Risks
-    vModMyoBaseNoVar=1e-10, # No Drug Risk of mild myopathy
-    vModMyoSimNoVar=0.00011,  # Simvastatin Mild Myopathy Baseline Risk
-    vModMyoSimMedVar=2.55,    # Rel Risk|Medium metabolizer
-    vModMyoSimPoorVar=9.56,   # Rel Risk|Poor metabolizer
-    vModMyoAltNoVar=0.00011,  # Alternate Drug Mild Myopathy Baseline Risk
-    vModMyoAltMedVar=1.08,    # Rel Risk|Medium metabolizer
-    vModMyoAltPoorVar=4.05,    # Rel Risk|Poor metabolizer
-
-    # 5-year Severe Myopathy Risks
-    vSevMyoBaseNoVar=1e-16,   # No Drug Risk of mild myopathy
-    vSevMyoSimNoVar=0.000034, # Simvastatin Mild Myopathy Baseline Risk
-    vSevMyoSimMedVar=2.55,    # Rel Risk|Medium metabolizer
-    vSevMyoSimPoorVar=9.56,   # Rel Risk|Poor metabolizer
-    vSevMyoAltNoVar=0.000034, # Alternate Drug Mild Myopathy Baseline Risk
-    vSevMyoAltMedVar=1.08,    # Rel Risk|Medium metabolizer
-    vSevMyoAltPoorVar=4.05,   # Rel Risk|Poor metabolizer
-    
-    vProbRahbdoDeath = 0.1, # Case fatality for severe myopathy
-    vProbcvdDeath = 0.117 # Case fatality for CVD
-
-)
-
-warfarin = list(
-  vPREDICTsens = 0.23,
-  vPREDICTspec = 0.93,
-
-  vProbabilityRead = 1.00, # probability of physician using test results
-  vProbabilityReactive = 1.00, # Under reactive, probability of ordering test
-  
-  # start warfarin
-  vpct_afib = 0.09, # add last observed % w/ a.fib among those on warfarin
-  #aLP_Warfarin = 1,
-  vshape_timetowarfarin = 0.66,
-  vscale_timetowarfarin = 33471.45,
-  
-  # INR: initial & time to get in range
-  vMedianTimetoINR	= 0.0239, 
-  vMedianTimetoINR_PGx	= 0.033,
-  vMedianTimetoINR_PGx_delay = 0.02888, 
-  vINRfreq = (read.csv("./warfarin/warfarin_inputs_INR.csv"))$INR_freq,
-  vINRvalue = (read.csv("./warfarin/warfarin_inputs_INR.csv"))$INR_value,
-  
-  # adverse events: bleed
-  vAF_Risk_Major_Bleed_3 = 0.01497, # risk of bleeding events for INR < 3 & AF indication
-  vAF_Risk_Major_Bleed_3to4 = 0.06224,
-  vAF_Risk_Major_Bleed_Over4 = 0.39118,
-  vRRMajorBleed_AF = 1,
-  vNonAF_Risk_Major_Bleed_3 = 0.01497,
-  vNonAF_Risk_Major_Bleed_3to4 = 0.06224,
-  vNonAF_Risk_Major_Bleed_Over4 = 0.39118, 
-  vRRMajorBleed_NonAF = 1,
-  
-  vAF_Risk_Minor_Bleed_3 = 0.0936, # risk of bleeding events for INR < 3 & AF indication (minor fix)
-  vAF_Risk_Minor_Bleed_3to4 = 0.3890,
-  vAF_Risk_Minor_Bleed_Over4 = 0.9999,
-  vRRMinorBleed_AF = 1,
-  vNonAF_Risk_Minor_Bleed_3 = 0.0936,
-  vNonAF_Risk_Minor_Bleed_3to4 = 0.3890,
-  vNonAF_Risk_Minor_Bleed_Over4 =	0.9999,
-  vRRMinorBleed_NonAF = 1,
-  
-  vTimeDurBleed = 365,
-
-  #adjusted estimates:
-  # V1:    90d risk is 1.95 * other time
-  # major: 0.01497 0.06224 0.39118 
-  # minor: 0.0973 0.4046 0.9999
-  # V2:    90d risk is 1.95 * average 1 yr
-  # major: 0.01852 0.07702 0.48409 
-  # minor: 0.1204 0.5007 0.9999
-  
-  ##prob distribution of six types of major bleeding events
-  vR_Bleed_ICH	= 0.144, 
-  vR_Bleed_ICH_Fatal = 0.156,
-  vR_Bleed_GI	= 0.557,
-  vR_Bleed_GI_Fatal	= 0.043,
-  vR_Bleed_Other = 0.098,
-  vR_Bleed_Other_Fatal = 0.002,
-  
-  # adverse event: stroke
-  vAF_Risk_Stroke_1.5 = 0.077,
-  vAF_Risk_Stroke_1.5to2 =	0.019,
-  vAF_Risk_Stroke_Over2 =	0.006,
-  vRRStroke_AF = 1,
-  vNonAF_Risk_Stroke_3 =	0.00001,
-  vNonAF_Risk_Stroke_Over3 =	0.006,
-  vRRStroke_NonAF = 1,
-  vTimeDurStroke = 365,
-  
-  ## prob distribution of three types of stroke events, among which major deficit stops warfarin and goes back to main model
-  vR_Stroke_MinorDeficit_2 = 0.4116,
-  vR_Stroke_MajorDeficit_2 = 0.4284,
-  vR_Stroke_Fatal_2 = 0.16,
-  vR_Stroke_MinorDeficit_Over2 = 0.5358,
-  vR_Stroke_MajorDeficit_Over2 = 0.4042,
-  vR_Stroke_Fatal_Over2 = 0.06,
-  
-  # adverse event: DVTPE, only for NonAF patients
-  vNonAF_Risk_DVTPE_2 = 0.019,
-  vNonAF_Risk_DVTPE_Over2 =	0.007,
-  vRRDVTPE_NonAF = 1,
-  vTimeDurDVTPE = 365,
-  
-  ## prob distribution of three types of DVTPE events
-  vR_DVT = 0.4,
-  vR_PE = 0.1,
-  vR_DVTPE_Fatal = 0.5
-)
-
 
 inputs <- list(
   # Population Parameters
@@ -341,33 +201,19 @@ inputs <- list(
   vSwitch = "None", # Can be one of following: "None", "All","Genotype"; trigger 30-day drug switch event
 
 # Control Which Drugs Are Run in the Model 
-  vDrugs       = list(vSimvastatin = FALSE, 
-                      vWarfarin = FALSE,
-                      vClopidogrel = TRUE),
+  vDrugs       = list(vSimvastatin = FALSE),
 
 # CURRENTLY PANEL IS FOR ALL DRUGS ???
-  vPanel       = list(vSimvastatin = TRUE, 
-                    vWarfarin=TRUE, 
-                    vClopidogrel = TRUE),
+  vPanel       = list(vSimvastatin = TRUE),
 
   # Drug specific model parameters
   clopidogrel = clopidogrel,
-  simvastatin = simvastatin,
-  warfarin    = warfarin,
   
   # If these names match the event names from the simmer model, then computation can be generalized!
   # These must be DAILY costs
 costs = list(
   panel_test      =   realdol(250,year=2012),
   single_test     =   realdol(100,year=2012),
-  mild_myopathy   =   realdol(129,year=2012),
-  mod_myopathy    =  realdol(2255/30,year=2012), # Note this divided by duration
-  sev_myopathy    = realdol(12811/30,year=2012),
-  rahbdo_death    = realdol(12811,year=2012), #pass on total sev_myopathy cost
-  cvd             = realdol(20347/30,year=2012),
-  cvd_death       = realdol(20347,year=2012), #pass on total cvd cost
-  simvastatin     =   realdol(147/365,year=2012),
-  alt_simvastatin = realdol(173.1/365,year=2012),
   
   aspirin         = realdol(2/30,year=2017),
   clopidogrel     = realdol(10/30,year=2017),
@@ -386,33 +232,11 @@ costs = list(
   revasc_cabg     = realdol(50560/14,2011),
   revasc_pci      = realdol(20670/7,2011),
   cabg_bleed      = realdol(35570/7,2011),
-  dapt_stroke  = realdol(21537,year=2007),
-  
-  warfarin        = realdol(71/90,year=2007),
-  #bleeding events: sharing w/ chlopidigrel model??
-  MajorBleed_ICH	= realdol(20740,year=2011),
-  MajorBleed_GI	  = realdol(2328/14,year=2011),
-  MajorBleed_Other = realdol(6154/14,year=2011),
-  MajorBleed_ICH_Fatal =	realdol(17920,year=2011),
-  MajorBleed_GI_Fatal =	realdol(17920,year=2011),
-  MajorBleed_Other_Fatal =	realdol(17920,year=2011),
-  MinorBleed = realdol(79/2,year=2011),
-  Stroke_MajorDeficit = realdol(21537,year=2007),
-  Stroke_MinorDeficit = realdol(15499,year=2007),
-  Stroke_Fatal    = realdol(10396,year=2007),
-  DVT	= realdol(7594,year=2004), 
-  PE =	realdol(13018,year=2004),
-  DVTPE_Fatal =	realdol(7000,year=2004),
-  out_of_range = realdol(29/3,year=2007),
-  in_range = realdol(29/7,year=2007)
+  dapt_stroke  = realdol(21537,year=2007)
   
 ),
 # Each listed duration will be corrected in the final data frame (temp disutility)
 durations = list(
-  mild_myopathy =  1,
-  mod_myopathy  = 30,
-  sev_myopathy  = 30,
-  cvd           = 30,
   
   bleed_ext_maj_nonfatal = 14,
   bleed_min_nonfatal = 2,
@@ -426,12 +250,6 @@ durations = list(
   
 ),
 disutilities = list(
-  mild_myopathy = 0.0100,
-  mod_myopathy  = 0.0500,
-  sev_myopathy  = 0.5300,
-  cvd           = 0.2445,
-  cvd_death     = 1,
-  rahbdo_death  = 1,
   
   
   bleed_ext_maj_nonfatal = .2,
@@ -449,35 +267,11 @@ disutilities = list(
   cabg_bleed      = .5,
   dapt_stroke  = 0.64,
   
-  MajorBleed_ICH	= 0.61,
-  MajorBleed_GI	  = 0.1511,
-  MajorBleed_Other = 0.1511,
-  MajorBleed_ICH_Fatal = 1, 
-  MajorBleed_GI_Fatal =	1, 
-  MajorBleed_Other_Fatal = 1,
-  MinorBleed      = 0.2,
-  Stroke_MajorDeficit = 0.64,
-  Stroke_MinorDeficit = 0.24,
-  Stroke_Fatal   = 1, 
-  DVT         	 = 0.79,
-  PE             = 0.79,
-  DVTPE_Fatal    = 1,
-  out_of_range = 0.012/3,
-  in_range = 0.012/7,
-  
   secular_death = 1
   
 ),
 # Each shows whether the event permanently decreases utility (type==0) vs temporarily(type==1)
 type= list(
-  mild_myopathy = 1,
-  mod_myopathy  = 1,
-  sev_myopathy  = 1,
-  cvd           = 1,
-  cvd_death     = 0,
-  rahbdo_death  = 0,
-  
-  
   bleed_ext_maj_nonfatal = 1,
   bleed_int_maj_nonfatal = 0,
   bleed_min_nonfatal = 1,
@@ -492,22 +286,6 @@ type= list(
   revasc_pci      = 1,
   cabg_bleed      = 1,
   dapt_stroke     = 0,
-  
-  MajorBleed_ICH	= 0,
-  MajorBleed_GI	  = 1,
-  MajorBleed_Other = 1,
-  MajorBleed_ICH_Fatal = 0, 
-  MajorBleed_GI_Fatal =	0, 
-  MajorBleed_Other_Fatal = 0,
-  MinorBleed      = 1,
-  Stroke_MajorDeficit = 0,
-  Stroke_MinorDeficit = 0,
-  Stroke_Fatal   = 0, 
-  DVT         	 = 0,
-  PE             = 0,
-  DVTPE_Fatal    = 0,
-  out_of_range = 1,
-  in_range = 1,
   
   secular_death = 0
   
