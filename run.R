d695025ed4dccde4d2b2f50a3878ffa1dd78b55c
Shawn Garbett <shawn.garbett@vanderbilt.edu>
2019-02-04 15:01:31 -0600

    Added sample space draw

--- run.R
+++ run.R
@@ -85,8 +85,66 @@ run.model <- function(inputs, strategy, seed)
   colSums(result)/runs
 }
 
+sample.point <- function(inputs, row)
+{
+  sample <- as.numeric(read.csv("sample.txt", header=FALSE)[row,])
+
+  inputs$clopidogrel$vRiskST30.Non                    <- sample[ 1]
+  inputs$clopidogrel$vRiskST365.Non                   <- sample[ 2]
+  inputs$clopidogrel$vRiskST30.Alt.Non                <- sample[ 3]
+  inputs$clopidogrel$vRiskST365.Alt.Non               <- sample[ 4]
+  inputs$clopidogrel$vRiskST30.LOF                    <- sample[ 5]
+  inputs$clopidogrel$vRiskST365.LOF                   <- sample[ 6]
+  inputs$clopidogrel$vRiskST30.Alt.LOF                <- sample[ 7]
+  inputs$clopidogrel$vRiskST365.Alt.LOF               <- sample[ 8]
+  inputs$clopidogrel$vSt.Case.Fatality.Non            <- sample[ 9]
+  inputs$clopidogrel$vSt.Case.Fatality.Alt.Non        <- sample[10]
+  inputs$clopidogrel$vSt.Case.Fatality.LOF            <- sample[11]
+  inputs$clopidogrel$vSt.Case.Fatality.Alt.LOF        <- sample[12]
+  inputs$clopidogrel$vPrCABG.ST                       <- sample[13]
+  inputs$clopidogrel$vRiskMI30.Non                    <- sample[14]
+  inputs$clopidogrel$vRiskMI365.Non                   <- sample[15]
+  inputs$clopidogrel$vRiskMI30.Alt.Non                <- sample[16]
+  inputs$clopidogrel$vRiskMI365.Alt.Non               <- sample[17]
+  inputs$clopidogrel$vRiskMI30.LOF                    <- sample[18]
+  inputs$clopidogrel$vRiskMI365.LOF                   <- sample[19]
+  inputs$clopidogrel$vRiskMI365.Alt.LOF               <- sample[20]
+  inputs$clopidogrel$vRiskMI365.Alt.LOF               <- sample[21]
+  inputs$clopidogrel$vPrCABG.MI                       <- sample[22]
+  inputs$clopidogrel$vPrPCI.MI                        <- sample[23]
+  inputs$clopidogrel$vRiskStroke30.Non                <- sample[24]
+  inputs$clopidogrel$vRiskStroke362.Non               <- sample[25]
+  inputs$clopidogrel$vRiskStroke30.Alt.Non            <- sample[26]
+  inputs$clopidogrel$vRiskStroke365.Alt.Non           <- sample[27]
+  inputs$clopidogrel$vRiskStroke30.LOF                <- sample[28]
+  inputs$clopidogrel$vRiskStrok365.LOF                <- sample[29]
+  inputs$clopidogrel$vRiskStroke30.Alt.LOF            <- sample[30]
+  inputs$clopidogrel$vRiskStroke365.Alt.LOF           <- sample[31]
+  inputs$clopidogrel$vRiskDeath                       <- sample[32]
+  inputs$clopidogrel$vRiskRV365                       <- sample[33]
+  inputs$clopidogrel$vPrCABG.RV                       <- sample[34]
+  inputs$clopidogrel$vRiskExtBleed                    <- sample[35]
+  inputs$clopidogrel$vRiskIntBleed                    <- sample[36]
+  inputs$clopidogrel$vRiskTIMIMinor                   <- sample[37]
+  inputs$clopidogrel$vRiskFatalBleed                  <- sample[38]
+  inputs$clopidogrel$vRR.ExtBleed.Ticagrelor          <- sample[39]
+  inputs$clopidogrel$vRR.TintBleed.Ticagrelor         <- sample[40]
+  inputs$clopidogrel$vRR.TIMIMinor.Ticagrelor         <- sample[41]
+  inputs$clopidogrel$vRR.FatalBleed.Ticagrelor        <- sample[42]
+  inputs$clopidogrel$vRiskCABGTIMImajor               <- sample[43]
+  inputs$clopidogrel$vRR.RiskCABGTIMImajor.Ticagrelor <- sample[44]
+  inputs$clopidogrel$vRR.Bleed.LOF                    <- sample[45]
+  inputs$costs$single_test                            <- sample[46]
+  inputs$costs$clopidogrel                            <- sample[47]
+  inputs$costs$ticagrelor                             <- sample[48]
+  
+  inputs
+}
+
 ignite <- function(inputs, seed, strategies=c(0, 2, 3))
 {
+  inputs <- sample.point(inputs, seed)
+  
   result <- unlist(lapply(strategies, function(x) run.model(inputs, x, seed)))
   #result <- unlist(mclapply(strategies, function(x) run.model(inputs, x, seed), mc.cores=4))
   names(result) <- paste0(c("dQALY", "dCOST"), rep(strategies, each=2))
