43fe0285c58059979b2bb1776f85742089a1e5a5
Shawn Garbett <shawn@garbett.org>
2019-02-05 08:32:44 -0600

    Fixed output bug on ACCRE

--- sim-accre.R
+++ sim-accre.R
@@ -3,7 +3,7 @@ source("run.R")
   #############################################################################
  ##
 ## ACCRE batch run
-args <- commandArgs(trailingOnly=TRUE)
-x    <- as.numeric(args[1])
-ignite(inputs, x)
+args    <- commandArgs(trailingOnly=TRUE)
+x       <- as.numeric(args[1])
+results <- ignite(inputs, x)
 save(results, file=paste0("output/run-",x,".Rdata"))
