44be0e4cc16d1bcc54043743c6db4f28d6e3b7d5
Shawn Garbett <shawn.garbett@vanderbilt.edu>
2019-02-19 11:28:59 -0600

    Getting prepped to rerun scenario 3

--- sim-accre.R
+++ sim-accre.R
@@ -5,5 +5,5 @@ source("run.R")
 ## ACCRE batch run
 args    <- commandArgs(trailingOnly=TRUE)
 x       <- as.numeric(args[1])
-results <- ignite(inputs, x)
+results <- ignite(inputs, x, strategies=3)
 save(results, file=paste0("output/run-",x,".Rdata"))
