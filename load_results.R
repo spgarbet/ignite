f1709c0a76f75872021d1ffe5f17e6f9fe35df4e
Shawn Garbett <shawn.garbett@vanderbilt.edu>
2019-02-20 10:03:20 -0600

    Fixed ordering of results

--- load_results.R
+++ load_results.R
@@ -1,3 +1,4 @@
+library(stringr)
 
 # Helper function for repeated operation over files.
 load.results <- function(target="output", verbose=FALSE)
@@ -8,8 +9,10 @@ load.results <- function(target="output", verbose=FALSE)
   do.call(rbind, lapply(files, function(f) {
     if(verbose) cat("Loading ", f, "\n")
     load(f)
+    
     results
   }))
 }
-results <- data.frame(load.results(verbose=TRUE))
+results <- data.frame(load.results(verbose=TRUE, target="output/20190219"))
+results$run <- as.numeric(gsub(".*run-?([0-9]+)\\.Rdata", "\\1", dir("output/20190219"), perl=TRUE))
 
